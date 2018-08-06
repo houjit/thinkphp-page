d89d0c3837bbe74603082bc72577166442e66ebf
houjit <447107108@qq.com>
2018-08-06 17:45:16 +0800

    Update Layui.php

--- Layui.php
+++ Layui.php
@@ -10,7 +10,7 @@ use think\Request;
  * 自定义扩展分页操作类
  * $total 总记录数  $limit 一页显示多少条
  * 数组调用：
-    \cocolait\bootstrap\page\Send::instance(['total' => 10, 'limit' => 10])->render($pages,$pageNums,$this->request->param());
+    \hou\page\Layui::instance(['total' => 10, 'limit' => 10])->render($pages,$pageNums,$this->request->param());
  * 参数介绍：
     $page 分页参数 $pageNums  总页数  第三参数是 分页额外参数 字符串或者数组
  *
